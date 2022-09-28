fdb51f8892cb488112b66d615814e57d33bd5814
Alexandreinfov <alexandrevalter88@gmail.com>
2022-09-28 11:21:03 -0300

    Aula de c# DIO

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ExemploExplorando.Models;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pr-BR");
+
+decimal valorMonetario = 1582.40M;
+
+Console.WriteLine($"{valorMonetario:C}");
+
+
+Pessoa p1 = new Pessoa("Leonardo", "Buta");
+// p1.Nome = "Alexandre";
+// p1.Sobrenome = "Valter";
+// p1.Idade = 20;
+
+Pessoa p2 = new Pessoa("Eduardo", "Neves Quiros");
+// p2.Nome = "Eduardo";
+// p2.Sobrenome = "Neves Queiros";
+// p2.Idade = 25;
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+// p1.Idade = 20;
+// p1.Apresentar();
