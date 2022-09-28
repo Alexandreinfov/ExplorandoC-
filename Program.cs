6fa241fd79cf56f751fcf06cb7c20554145909cd
Alexandreinfov <alexandrevalter88@gmail.com>
2022-09-28 13:01:45 -0300

    Lendo arquivos txt

--- Program.cs
+++ Program.cs
@@ -1,30 +1,55 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pr-BR");
 
-decimal valorMonetario = 1582.40M;
+//Leitrura de arquivo txt 
 
-Console.WriteLine($"{valorMonetario:C}");
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
 
-Pessoa p1 = new Pessoa("Leonardo", "Buta");
+foreach (string linha in linhas)
+{
+  Console.WriteLine(linha);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pr-BR");
+
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine($"{valorMonetario:C}");
+
+
+// Pessoa p1 = new Pessoa("Leonardo", "Buta");
 // p1.Nome = "Alexandre";
 // p1.Sobrenome = "Valter";
 // p1.Idade = 20;
 
-Pessoa p2 = new Pessoa("Eduardo", "Neves Quiros");
+// Pessoa p2 = new Pessoa("Eduardo", "Neves Quiros");
 // p2.Nome = "Eduardo";
 // p2.Sobrenome = "Neves Queiros";
 // p2.Idade = 25;
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 // p1.Idade = 20;
 // p1.Apresentar();
